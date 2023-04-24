8c459944d9c91d0dcbc25143a067ea4fc5edfb9e
ozeke2186 <108537228+ozeke2186@users.noreply.github.com>
2023-04-24 10:42:07 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,85 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Question {
+private:
+    int questionID;
+    string text;
+    vector<string> options;
+    string answer;
+
+public:
+    Question(int id, string t, vector<string> opt, string ans) {
+        questionID = id;
+        text = t;
+        options = opt;
+        answer = ans;
+    }
+
+    int getQuestionID() {
+        return questionID;
+    }
+
+    string getText() {
+        return text;
+    }
+
+    vector<string> getOptions() {
+        return options;
+    }
+
+    string getAnswer() {
+        return answer;
+    }
+};
+
+class Quiz {
+private:
+    int quizID;
+    string title;
+    vector<Question> questions;
+
+public:
+    Quiz(int id, string t, vector<Question> ques) {
+        quizID = id;
+        title = t;
+        questions = ques;
+    }
+
+    int getQuizID() {
+        return quizID;
+    }
+
+    string getTitle() {
+        return title;
+    }
+
+    vector<Question> getQuestions() {
+        return questions;
+    }
+};
+
+class QuestionBank {
+private:
+    vector<Question> questions;
+
+public:
+    QuestionBank(vector<Question> ques) {
+        questions = ques;
+    }
+
+    vector<Question> getQuestions() {
+        return questions;
+    }
+};
